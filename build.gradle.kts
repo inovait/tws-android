e92fe5cb0cbe5b0e36c704d186058bb9113f56bc
Tilen Pintarić <tilen.pintaric@inova.si>
2024-07-10 09:53:54 +0200

    fix(github): added nexus plugin for release

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,1 +1,14 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+plugins {
+   alias(libs.plugins.nexusPublish)
+}
+
+if (properties.containsKey("ossrhUsername")) {
+   nexusStaging {
+      username = property("ossrhUsername") as String
+      password = property("ossrhPassword") as String
+      packageGroup = "si.inova"
+      stagingRepositoryId = property("ossrhRepId") as String
+   }
+}
