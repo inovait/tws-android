ad6d4295dbb93be39be352d9fc3cb12f0c07d6a2
Tilen Pintarić <tilen.pintaric@inova.si>
2024-07-11 08:51:48 +0200

    fix(core): test for release

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -39,7 +39,6 @@ dependencies {
     implementation(libs.androidx.activity.compose)
     implementation(libs.kotlin.immutableCollections)
     implementation(libs.androidx.compose.ui.tooling.preview)
-
     implementation(libs.compose.foundation)
     implementation(libs.androidx.compose.material3)
     implementation(libs.timber)
