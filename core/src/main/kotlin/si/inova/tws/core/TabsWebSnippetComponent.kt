bec0306faaf8a510c97b017b330510a3048bd714
Tilen Pintarić <tilen.pintaric@inova.si>
2024-09-04 14:24:18 +0200

    test(core): show popup interstitial

--- core/src/main/kotlin/si/inova/tws/core/TabsWebSnippetComponent.kt
+++ core/src/main/kotlin/si/inova/tws/core/TabsWebSnippetComponent.kt
@@ -16,6 +16,9 @@
 
 package si.inova.tws.core
 
+import android.app.Activity
+import android.content.Context
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.BackHandler
 import androidx.compose.animation.Crossfade
 import androidx.compose.foundation.Image
@@ -27,7 +30,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.ScrollableTabRow
@@ -39,17 +46,23 @@ import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.LoadAdError
+import com.google.android.gms.ads.interstitial.InterstitialAd
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.persistentListOf
 import si.inova.tws.core.data.WebSnippetData
@@ -137,6 +150,8 @@ fun TabsWebSnippetComponent(
             tabIndex = if (targets.size <= mainTabIndex) 0 else mainTabIndex
         }
 
+        val context = LocalContext.current
+        val activity = LocalContext.current as? ComponentActivity
         Scaffold(
             modifier = modifier,
             topBar = {
@@ -154,6 +169,15 @@ fun TabsWebSnippetComponent(
                         tabIndex = it
                     }
                     lastSelectedTabIndex.intValue = it
+                }
+            },
+            floatingActionButton = {
+                FloatingActionButton(onClick = {
+                    activity?.let {
+                        showInterstialAd(context, activity)
+                    }
+                }) {
+                    Icon(Icons.Default.Add, "add")
                 }
             }
         ) { padding ->
@@ -199,6 +223,22 @@ fun TabsWebSnippetComponent(
             }
         }
     }
+}
+
+private fun showInterstialAd(context: Context, activity: Activity) {
+    InterstitialAd.load(
+        context,
+        "ca-app-pub-3940256099942544/1033173712", //Change this with your own AdUnitID!
+        AdRequest.Builder().build(),
+        object : InterstitialAdLoadCallback() {
+            override fun onAdFailedToLoad(adError: LoadAdError) {
+            }
+
+            override fun onAdLoaded(interstitialAd: InterstitialAd) {
+                interstitialAd.show(activity)
+            }
+        }
+    )
 }
 
 @Composable
