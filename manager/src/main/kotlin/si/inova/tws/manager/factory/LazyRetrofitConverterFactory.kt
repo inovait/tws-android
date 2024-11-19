24b6f5a4bf5d1808679f14b05555e062c181be51
Žan Bezjak <zan.bezjak@inova.si>
2024-11-19 13:29:52 +0100

    style(manager): requireNotNull with error

--- manager/src/main/kotlin/si/inova/tws/manager/factory/LazyRetrofitConverterFactory.kt
+++ manager/src/main/kotlin/si/inova/tws/manager/factory/LazyRetrofitConverterFactory.kt
@@ -57,14 +57,12 @@ internal class LazyRetrofitConverterFactory(private val parentFactory: Lazy<Conv
     ): Converter<*, RequestBody> {
         val lazyConverter = lazy {
             @Suppress("UNCHECKED_CAST")
-            requireNotNull(
-                parentFactory.value.requestBodyConverter(
-                    type,
-                    parameterAnnotations,
-                    methodAnnotations,
-                    retrofit
-                ) as Converter<Any?, RequestBody>
-            ) { "Moshi converter should never be null" }
+            parentFactory.value.requestBodyConverter(
+                type,
+                parameterAnnotations,
+                methodAnnotations,
+                retrofit
+            ) as? Converter<Any?, RequestBody> ?: error("Moshi converter should never be null")
         }
 
         return Converter<Any?, RequestBody> { value -> lazyConverter.value.convert(value) }
