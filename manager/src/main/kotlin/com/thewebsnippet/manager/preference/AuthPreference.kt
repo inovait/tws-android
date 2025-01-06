d83686a94d7c74d13885422ab4dd9ffb39373cc2
Tilen Pintarić <tilen.pintaric@inova.si>
2025-01-06 14:31:28 +0100

    refactor(manager): created AuthPreference interface

--- manager/src/main/kotlin/com/thewebsnippet/manager/preference/AuthPreference.kt
+++ manager/src/main/kotlin/com/thewebsnippet/manager/preference/AuthPreference.kt
@@ -15,90 +15,16 @@
  */
 package com.thewebsnippet.manager.preference
 
-import android.annotation.SuppressLint
 import android.content.Context
-import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.core.edit
-import androidx.datastore.preferences.core.stringPreferencesKey
-import androidx.datastore.preferences.preferencesDataStore
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.runBlocking
 
-@SuppressLint("DiscouragedApi") // RESOURCE_VALUE_NAME can only be retrieved from build
-internal object AuthPreference {
-    private lateinit var appContext: Context
+internal interface AuthPreference {
+    fun safeInit(context: Context)
 
-    private val Context.authPreferences: DataStore<Preferences> by preferencesDataStore(name = DATASTORE_NAME)
+    val jwt: String
+    val refreshToken: Flow<String>
+    val accessToken: Flow<String>
 
-    fun safeInit(context: Context) {
-        if (::appContext.isInitialized) return
-
-        appContext = context
-
-        runBlocking {
-            if (storedJwt.first() != jwt && storedJwt.first().isNotEmpty()) {
-                clearPreferences()
-            }
-
-            setJWT(jwt)
-        }
-    }
-
-    val jwt: String by lazy {
-        appContext.getString(appContext.resources.getIdentifier(RESOURCE_VALUE_NAME, "string", appContext.packageName))
-    }
-
-    val refreshToken: Flow<String> by lazy {
-        appContext.authPreferences.data
-            .map { preferences ->
-                preferences[REFRESH_TOKEN] ?: ""
-            }
-    }
-
-    val accessToken: Flow<String> by lazy {
-        appContext.authPreferences.data
-            .map { preferences ->
-                preferences[ACCESS_TOKEN] ?: ""
-            }
-    }
-
-    suspend fun setRefreshToken(refreshToken: String) {
-        appContext.authPreferences.edit { settings ->
-            settings[REFRESH_TOKEN] = refreshToken
-        }
-    }
-
-    suspend fun setAccessToken(accessToken: String) {
-        appContext.authPreferences.edit { settings ->
-            settings[ACCESS_TOKEN] = accessToken
-        }
-    }
-
-    private val storedJwt: Flow<String> by lazy {
-        appContext.authPreferences.data
-            .map { preferences ->
-                preferences[JWT] ?: ""
-            }
-    }
-
-    private suspend fun setJWT(jwt: String) {
-        appContext.authPreferences.edit { settings ->
-            settings[JWT] = jwt
-        }
-    }
-
-    private suspend fun clearPreferences() {
-        appContext.authPreferences.edit {
-            it.clear()
-        }
-    }
+    suspend fun setRefreshToken(refreshToken: String)
+    suspend fun setAccessToken(accessToken: String)
 }
-
-private const val RESOURCE_VALUE_NAME = "com.thewebsnippet.service.jwt"
-private const val DATASTORE_NAME = "authPreferences"
-private val JWT = stringPreferencesKey("jwt")
-private val REFRESH_TOKEN = stringPreferencesKey("refreshToken")
-private val ACCESS_TOKEN = stringPreferencesKey("accessToken")
