91ca8d91cb8d15e4cad2556b5b40a2ab8e8636e1
Tilen Pintarić <tilen.pintaric@inova.si>
2025-03-17 12:50:23 +0100

    chore(github): remove github release

--- buildSrc/src/main/kotlin/util/Publishing.kt
+++ buildSrc/src/main/kotlin/util/Publishing.kt
@@ -83,10 +83,19 @@ fun Project.configureForJReleaser() {
     if (!properties.containsKey("mavenUsername")) return
 
     extensions.configure<JReleaserExtension>("jreleaser") {
+        release {
+            github {
+                enabled.set(false)
+                skipRelease.set(true)
+                skipTag.set(true)
+            }
+        }
+
         signing {
             active.set(Active.ALWAYS)
             armored.set(true)
             mode.set(Signing.Mode.FILE)
+            gitRootSearch.set(true)
             publicKey.set(property("publicKeyPath") as String)
             secretKey.set(property("privateKeyPath") as String)
         }
