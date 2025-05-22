bba5a4a83aaa38582cd11e86b44c27a2b084cc19
Zan <zan.bezjak@inova.si>
2025-05-22 13:33:07 +0200

    fix(sample): add error message text alignment

--- sample/src/main/kotlin/com/thewebsnippet/sample/components/ErrorView.kt
+++ sample/src/main/kotlin/com/thewebsnippet/sample/components/ErrorView.kt
@@ -34,6 +34,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -67,7 +68,8 @@ internal fun ErrorView(
 
         Text(
             text = errorText,
-            style = TextStyle(fontSize = 20.sp, fontWeight = FontWeight.Medium)
+            style = TextStyle(fontSize = 20.sp, fontWeight = FontWeight.Medium),
+            textAlign = TextAlign.Center
         )
 
         refresh?.let {
